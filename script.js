1bf89ecdfa03b0be42ffbc6e0236de2381373f77
hectorsantana-cell <hector.santana@escola.pr.gov.br>
2026-09-17 09:40:58 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+let nota1trim;
+let nota2trim;
+let resultado;
+
+function calcular() {
+    nota1trim = Number (prompt("Digite a nota do primeiro trimestre"));
+    nota2trim = Number (prompt("Digite a nota do segundo trimestre"));
+    resultado = 180 - nota1trim + nota2trim;
+    
+}
