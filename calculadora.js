8accd3161ea97d7368e2a9b4e81c344a8c8558c0
hectorsantana-cell <hector.santana@escola.pr.gov.br>
2026-09-21 09:43:55 -0300

    Add files via upload

---
+++ calculadora.js
@@ -0,0 +1,9 @@
+let valor1;
+let valor2;
+let resultado;
+
+function Calcular() {
+    let valor1 = Number (prompt("digite primeiro valor"));
+    let valor2 = Number(prompt("digite segundo valor"));
+    alert ("O resultado é: " + resultado);
+}
